150a1421f88f27bbad89eaabf6411f815944fe38
zhangyingyu <1047714069@qq.com>
2020-08-08 13:57:22 +0800

    去掉路由

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,6 @@ import achievementRouter from './model/achievement-router-config'
 import homeRouter from './model/home-router-config'
 import rosterRouter from './model/roster-router-config'
 import informationRouter from './model/information-router-config'
-import attendanceRouter from './model/attendance-router-config'
 
 import notFound from "../view/public/notFound"
 
@@ -19,6 +18,6 @@ export default new Router ({
                         path:'/expenditure',
                         component:notFound
                 },
-                flowRouter,achievementRouter,homeRouter,rosterRouter,informationRouter,attendanceRouter
+                flowRouter,achievementRouter,homeRouter,rosterRouter,informationRouter
         ]
 })
