43cdc1579da6dc35fe04daba037c028215ea99f9
zhangyingyu <1047714069@qq.com>
2020-08-10 10:23:52 +0800

    滚动条修改

--- src/view/roster/team/team.vue
+++ src/view/roster/team/team.vue
@@ -2,7 +2,7 @@
         <!-- <div class="">
             这里是班组列表信息
         </div> -->
-        <div class="info-wrap">
+        <div class="team-wrap">
             <router-view></router-view>
         </div>
 </template>
@@ -12,11 +12,12 @@
 </script>
 
 <style lang="stylus" scoped>
-    .info-wrap{
+    .team-wrap{
         padding 0 10px 10px 0px;
         width 100%;
-        height: calc(100vh - 112px);
-        overflow-y auto
+        // height: calc(100vh - 112px);
+        min-height 100%;
+        overflow-y auto;
         p{
             font-size 16px;
             line-height 36px;
